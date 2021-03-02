61adf4cce28adaa519201c912a083548e9e3a133
Elena Surilova <elen21surilova@gmail.com>
2021-03-02 17:51:57 +0200

    Added the seventh homework, but now without the fifth task

---
+++ hw7-surilova/script.js
@@ -0,0 +1,87 @@
+// Функция возвращает true если символ найден в строке и false если нет
+function isSymbolPresentInString(str, symbol) {
+	for (let strSymb of str) {
+		if (strSymb === symbol) {
+			return true;
+		}
+	}
+	return false;
+}
+// console.log(isSymbolPresentInString("Hello World!", "W"));
+
+// Функция возвращает индекс первого найденного символа в строке, или -1 если символ не найден
+function getSymbolIndex(str, symbol) {
+	for (let i = 0; i < str.length; i++) {
+		if (str[i] === symbol) {
+			return i;
+		}
+	}
+	return -1;
+}
+// console.log(getSymbolIndex("Hello World!", "W"));
+// console.log(getSymbolIndex("Hello World!", "b"));
+
+// Функция копирует свойства из объекта origin в объект target и возвращает его
+function copy(target, origin) {
+	for (let key in origin) {
+		target[key] = origin[key];
+	}
+	return target;
+}
+const user = {
+	name: "John",
+	age: 18,
+	"isAdmin": false
+};
+const admin = copy({}, user);
+
+// Функция сравнивает два объекта
+function compare(obj1, obj2) {
+	if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+		return false;
+	}
+	const arrKeys1 = Object.keys(obj1);
+	const arrKeys2 = Object.keys(obj2);
+	const arrValue1 = Object.values(obj1);
+	const arrValue2 = Object.values(obj2);
+
+	for (let i = 0; i < arrKeys1.length; i++) {
+		if (arrKeys1[i] !== arrKeys2[i]) {
+			return false;
+		}
+	}
+	for (let i = 0; i < arrValue1.length; i++) {
+		if (arrValue1[i] !== arrValue2[i]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+// admin.name = "Joe";
+console.log(compare(user, admin));
+
+// Функция анализирует строку и возвращает данные о том, сколько раз встретилась та или иная буква
+function analizeString(str) {
+	const result = {};
+	// let newSymb = str[0];
+	for (let i = 0; i < str.length; i++) {
+		let counter = 0;
+		let newSymb = str[i];
+		if (newSymb === str[i]) {
+			counter++;
+			result[newSymb] = counter;
+		}
+		if (newSymb !== str[i]) {
+			// counter = 0;
+			// for (let j = i + 1; j < str.length - i; j++) {
+			newSymb = str[i + 1];
+			counter++;
+			result[newSymb] = counter;
+			// }
+		}
+		console.log(counter);
+	}
+	console.log(result);
+}
+analizeString("dsldkjrirdxmnaejehtirtgask");
