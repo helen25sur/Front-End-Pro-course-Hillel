1202a6ac64fe07cce54784c9a2efa6893e788381
Elena Surilova <elen21surilova@gmail.com>
2021-05-16 13:39:09 +0300

    start setting for webpack

---
+++ finally-project/src/index.js
@@ -0,0 +1,7 @@
+import './styles/style.css'
+
+// console.log('Hello world')
+
+const h1 = document.createElement('h1');
+h1.innerText = 'Finally project';
+document.body.appendChild(h1);
---
+++ finally-project/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'main-[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, "./dist"),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] },
+      { test: /\.html$/i, loader: 'html-loader', },
+    ],
+
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'webpack Boilerplate',
+      template: path.resolve(__dirname, './src/template.html'),
+      filename: 'index.html',
+  }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+};
