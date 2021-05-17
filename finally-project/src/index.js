658c4d68a51a1bbba1efcf07bf905f548599a2c3
Elena Surilova <oblap-lena@yandex.ru>
2021-05-17 12:56:03 +0300

    add greeting page

--- finally-project/src/index.js
+++ finally-project/src/index.js
@@ -1,7 +1,1 @@
-import "./styles/style.css";
-
-// console.log('Hello world')
-
-const h1 = document.createElement("h1");
-h1.innerText = "Finally project";
-document.body.appendChild(h1);
+import './style.css';
