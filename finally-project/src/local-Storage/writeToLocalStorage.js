e4d4d8debcabddc68d24359a7d3937e25fc7a06c
helen25sur <65970892+helen25sur@users.noreply.github.com>
2021-06-11 12:52:53 +0300

    Revert "Finally project"

--- finally-project/src/local-Storage/writeToLocalStorage.js
+++
@@ -1,8 +0,0 @@
-export function writeToLocalStorage(arrData) {
-  for (let i = 0; i < arrData.length; i++) {
-    const keyMovie = Object.keys(arrData[i]);
-    keyMovie.forEach((key) => {
-      localStorage.setItem(`${arrData[i].id}:${key}`, arrData[i][key]);
-    });
-  }
-}
